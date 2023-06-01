47d7a84990d697f1656fed2ca254238427cc9eb8
Eph97 <ephraim.sutherland@yale.edu>
2023-06-01 14:18:24 -0400

    made new batch script

--- code/make_batch.py
+++ code/make_batch.py
@@ -6,7 +6,11 @@ import numpy as np
 # for p in np.linspace(0.1, 0.9, 1):
 options = np.linspace(0.0, 1.0, 11)
 
+count = 0
 for i in options:
     for j in options:
         # fix above print but limit decimals to two places
-        print("python simulate-inter.py", 1, round(i, 2), 1, round(j, 2), ">> output.csv")
+        i = round(i,2)
+        j = round(j,2)
+        print("python simulate-inter.py", 1, i, 1, j, f"> ../output/output{count}.csv")
+        count +=1

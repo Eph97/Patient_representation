e3624b2923ce398b87c3ba326a952ab85039d692
Eph97 <ephraim.sutherland@yale.edu>
2023-07-20 11:02:45 -0400

    updated writeup

--- code/version2/optimal-prop.py
+++ code/version2/optimal-prop.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
-from errorv2.py import *
+import numpy as np
+from errorv2 import *
 
 v0 = 1
 v1 = 1
+
+options = np.linspace(0.0, 1.0, 51)
 # p = 0.02
 # y = [errorMenWom10test(p, x, 0.5,3) for x in options]
-y = [justMen(p, v0,v1)[0] for p in options[1:]]
+y = [opt_prop(p, v0,v1)[0] for p in options[1:]]
 plt.plot(options[1:], y)
 plt.plot(options[1:], options[1:], color="red")
 plt.ylabel("Trial Proportion")
